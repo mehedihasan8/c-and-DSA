aed2a5718d5e295a50e7ae5a4d94ceda51edec67
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-06-04 22:18:45 +0600

    min in array of object

---
+++ module 7/min_in_array_of_object.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student {
+    public:
+        string name;
+        int roll;
+        int marks;
+};
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    Student a[n];
+
+    for(int i = 0; i <n; i++){
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+
+    Student min;
+
+    min.marks = INT_MAX;
+
+
+    for(int i = 0; i < n; i++){
+        if(a[i].marks < min.marks){
+            min = a[i];
+        }
+    }
+
+    cout << min.name << " " << min.roll << " " << min.marks << endl;
+
+    return 0;
+}
